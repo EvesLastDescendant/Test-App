7ca3e56fa1b97d344650458619ff011f9a56083a
EvesLastDescendant <clydewitoutabonnie@gmal.com>
2024-05-15 11:28:15 +0100

    revamped

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 // import { useState, useEffect } from "react"
 
+import ReactCurvedText from "react-curved-text"
+
 const IntroComponent = (props) => {
     // const [images, setImages] = useState([
     //     { id: 1, src: '/imgs/student-1.png', alt: 'Image 1', display: 'none' },
@@ -20,32 +22,40 @@ const IntroComponent = (props) => {
 
     return (
         <>
-            <div className=''>
-                {/* <div className="flex flex-col md:flex-row items-center justify-center image-box">
-                    {
-                        images.map((image) => (
-                            <img key={image.id} src={image.src} alt={image.alt} width={200} style={{display: image.display}} />
-                        ))
-                    }
-                </div> */}
-                <div className="flex items-center justify-center h-72">
-                    <img src="/imgs/student-1.png" alt="work" srcSet="/imgs/student-1.png, /imgs/student-2.png, /imgs/student-3.png" width={500} />
+            <div className='w-3/4 h-1/2 flex flex-col md:flex-row items-center justify-between'>
+                <div className="relative flex flex-col items-center">
+                    <img src="/imgs/student-1.png" alt="student-test" className="absolute" />
+                    <ReactCurvedText 
+                        width={200}
+                        height={200}
+                        cx={100}
+                        cy={100}
+                        rx={70}
+                        ry={70}
+                        startOffset={29}
+                        reversed={false}
+                        text="SOFTWARE DEV CBT"
+                        textProps={{style: {fontSize: 'var(--fs-lg)'}}}
+                        textPathProps={{fill: '#1d4ed8'}}
+                        tspanProps={{dy: 29}}
+                        ellipseProps={null}
+                        svgProps={null}
+                    />
                 </div>
-                <h1 className="mx-auto text-center mb-3 font-bold text-white intro-text">SOFTWARE DEVELOPMENT ASSESSMENT</h1>
-                <p className="text-pink-200 instruction-text">Please fill in the form below before procedding to the test</p>
-                <form className="p-2" onSubmit={props.handleSubmit}>
-                    <div className="flex flex-col md:flex-row justify-between items-center">
-                        <div className="mb-4 md:mb-0 md:mr-4">
-                            <input type="text" name="name" value={props.userData.name} placeholder="Fullname" onChange={props.handleOnchange} className="text-slate-700 rounded-md ps-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" autoComplete="true" />
+                
+                <div className="w-full md:w-3/5 flex flex-col gap-5">
+                    <p className="text-blue-600 instruction-text">Fill in form then proceed</p>
+                    <form className="shadow-lg bg-zinc-100 rounded-md" onSubmit={props.handleSubmit}>
+                        <div className="flex flex-col md:flex-row justify-between items-center p-4">
+                            <div className="mb-4 md:mb-0 md:mr-4">
+                                <input type="text" name="name" value={props.userData.name} placeholder="your name" onChange={props.handleOnchange} className="text-slate-700 border-blue-400 border rounded-md ps-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" autoComplete="true" />
+                            </div>
+                            <div>
+                                <input type="submit" value="To Test" className="text-slate-100 w-28 px-3 py-2 rounded-sm bg-gradient-to-r from-blue-700 to-blue-400 mx-auto flex justify-center items-center toggle-button" />
+                            </div>
                         </div>
-                        <div className="mb-4 md:mb-0 md:mr-4">
-                            <input type="text" name="gender" value={props.userData.gender} placeholder="Gender" onChange={props.handleOnchange} className="text-slate-700 rounded-md ps-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" autoComplete="true" />
-                        </div>
-                        <div>
-                            <input type="submit" value="To Test" className="text-slate-100 w-28 px-3 py-2 rounded-md bg-gradient-to-r from-violet-500 to-fuchsia-500 mx-auto flex justify-center items-center toggle-button" />
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </>
     )
--- src/components/res.jsx
+++ src/components/res.jsx
@@ -1,18 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons"
+
 /* eslint-disable react/prop-types */
 const Result = (props) => {
     return (
         <>
-            <div className="mx-auto shadow-lg flex flex-col justify-between items-center py-5 ">
-                <p className="text-extrabold res">Assessment Complete</p>
-                
+            <div className="mx-auto bg-gray-50 shadow-lg flex flex-col justify-between items-center p-14">
+                <p className="text-extrabold text-red-400 res">Assessment Complete</p>
+                {/**add tick icon */}
+                <FontAwesomeIcon icon={faCircleCheck} size="6x" color="rgb(74, 222, 128)" />
                 <table className="w-full text-center mx-auto">
                     <thead>
                         <tr>
-                        <th className="result-text">Student Name: </th>
-                        <th className="result-text">{props.userData[0].name}</th>
+                            <th className="result-text text-blue-400">Student Name: </th>
+                            <th className="result-text text-blue-400">{props.userData[0].name}</th>
                         </tr>
                     </thead>
                 </table>
+                <div>Score: {props.testScore}</div>
                 <div className="mt-5 w-2/3">{props.getProgressColor(props.testScore)}</div>
             </div>
         </>
